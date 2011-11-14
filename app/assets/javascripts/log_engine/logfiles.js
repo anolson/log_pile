5528e51a7c3e2edfc95c5b2ae0e1607e24a5a7c7
Andrew Olson <anolson@gmail.com>
2011-11-14 16:27:31 -0500

    UI improvements.

--- app/assets/javascripts/log_engine/logfiles.js
+++ app/assets/javascripts/log_engine/logfiles.js
@@ -1,15 +1,42 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var intervalId;
+
 $(document).ready(function() {
   fetchLogMessages();
-  setInterval(fetchLogMessages, 10000);
+  appendLogMessages();
+  
+  $("#append-log-messages").click(function() {
+    appendLogMessages();
+  });
 });
 
 function fetchLogMessages() {
-  key = $("#messages").data("stream-key");
-  if(key) {
-    $.get(this.location + '/messages?key=' + key, function(data) {
+  path = $("#messages").data("poller-location");
+  if(path) {
+    $("#loader").show();
+    $.get(path, function(data) { 
       $("#messages").append(data);
-    });    
+    });
+    $("#loader").hide();
+    scrollLogMessages();
+  }
+}
+
+function appendLogMessages() {
+  if($("#append-log-messages").is(":checked")) {
+    intervalId = setInterval(fetchLogMessages, 10000);    
+  }
+  else {
+    clearInterval(intervalId);
+  }
+}
+
+function scrollLogMessages() {
+  // Don't if we have scrolled earlier in the log messages.
+  if($(window).scrollTop() + $(window).height() ==  $(document).height()) {
+    $("body").animate({ 
+      scrollTop: $(document).height() 
+    }, 1000);      
   }
 }
