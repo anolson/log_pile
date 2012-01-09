7d8b038604d830e7b47e7ec42aa424d2c568d795
Andrew Olson <anolson@gmail.com>
2012-01-09 17:00:23 -0500

    Generate logfile permalink

--- app/models/log_pile/logfile.rb
+++ app/models/log_pile/logfile.rb
@@ -1,11 +1,20 @@
 module LogPile
   class Logfile < ActiveRecord::Base
+    validates_presence_of :name, :path
+
+    after_create :generate_permalink
+
     def to_param
       permalink
     end
-    
+
     def file_last_modified
       File.stat(path).mtime
+    end
+
+    def generate_permalink
+      self.permalink = name.gsub(/\s/, '_').downcase
+      save
     end
   end
 end
